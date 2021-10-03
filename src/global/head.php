0c8d86a9d6248c39d210d04836059314d7a9dc90
Brouart Arthur <arthur.brouart@etu.enseeiht.fr>
2021-10-03 22:00:45 +0200

    fix head

--- src/global/head.php
+++ src/global/head.php
@@ -14,22 +14,9 @@
 </head>
 <body>
 
-<script>
-    window.onscroll = function() {scrollFunction()};
-    // Changer la taille de l'entete on scroll
-    function scrollFunction() {
-        return ; // On annule la fonction
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            document.getElementById("titre-site").style.fontSize = "var(--main-title-small-size)";
-        } else {
-            document.getElementById("titre-site").style.fontSize = "var(--main-title-size)";
-        }
-    }
-</script>
-
 <div id="menu">
     <!-- Le titre du site web -->
-    <h1 id="titre-site">Atelier iMage</h1>
+    <h1 id="titre-site">Atelier Mage</h1>
 
     <!-- Les liens vers les autres pages -->
     <div id="links">
